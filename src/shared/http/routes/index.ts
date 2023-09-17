976cd520ab7ea8a7353f1c546da0e8fcb8d415ac
Kaian Samuel <kaiansamuelster@gmail.com>
2023-09-17 08:32:01 -0300

    Add roles routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import { rolesRouter } from '@roles/http/routes/roles.routes'
+
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Olá Dev' })
+  return response.json({ message: 'Olá Dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
